cdda115f4de1126b6cc86947f604b6ab59b7266a
kimts2627 <kimtstststs@gmail.com>
2021-06-10 22:50:46 +0900

    [Frontend] <feature>: setting up prettier and eslint
    prettier와 eslint셋팅
    
    next custome document 파일 생성

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/style.scss'
-import 'tailwindcss/tailwind.css'
+import "../styles/style.scss";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) { 
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
